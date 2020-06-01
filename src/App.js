e1ea5bce9150c4e6539eaaea0a7888417336624f
FirozKabir65 <firoj.kabir007@gmail.com>
2020-06-02 00:55:41 +0200

    API key and link, form for search

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	
+	const APP_ID = "607bf35f";
+	const APP_KEY = "097c3060148835a83c93927fd7e44c55";
+
+	const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+	const [counter, setCounter] = useState(0);
+	
+	useEffect(() => {
+		console.log('Effect has been run');
+	}, []);
+
+
+
+	return (
+		<div className="App">
+			<form className="search-form">
+				<input className="search-bar" type="text"/>
+				<button 
+					className="search-btn" type="submit">
+					Search
+				</button>
+			</form>
+			<h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+		</div>
+	);
+};
 
 export default App;
